8f94d24569c41e63a2b45c6b6a2ac95d284ce3bb
Vibha Alaguraj <141651144+FrostedParadigm14@users.noreply.github.com>
2026-04-24 22:45:21 +0530

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = defaultdict(list)
+
+        for s in strs:
+            freq = [0] * 26 
+            for char in s:
+                freq[ord(char) - ord('a')]+=1
+            
+            key = str(freq)
+            dict[key].append(s)
+
+        #print(dict.values())
+        return list(dict.values())
+
+
+'''
+defaultdict to make dict of list, tuple, set, etc...
+   - make a new freq list of 0 from 0 to 26....for each word - use that as key
+   - if the letter - 'a' is there then +1 to that index
+ord is to get unicode/ascii value 
+    - use the freq as a tuple to add as key to dict
+    - append the word, so multiple values added to one key
+    - python normally not allow list/arr as key bc its not immutable, change to tuple
+NO add in append, extend will create 'c', 'a', 't'
+    - make a list of the values in dictionary
+    - return list
+
+use tuple or string, not array/list as key
+
+'''
+        
